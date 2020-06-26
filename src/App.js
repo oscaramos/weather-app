6e989dc5fd4afdd523f328421771e5add0aedd1a
Oscar <oscar.ramos@ucsp.edu.pe>
2020-06-26 11:06:07 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles(theme => ({
+  weatherInfoContainer: {
+    background: 'linear-gradient(190deg, rgba(10,129,187,1) 0%, rgba(0,197,139,1) 100%)'
+  },
+  location: {
+    fontFamily: "NotoSans",
+    fontWeight: "600",
+    fontSize: '1.3em',
+    color: 'white',
+  },
+  currentTime: {
+    fontFamily: "NotoSans",
+    fontWeight: "400",
+    fontSize: '1em',
+    color: 'white',
+  },
+  temperature: {
+    fontFamily: "NotoSans",
+    fontWeight: "100",
+    fontSize: '7em',
+    color: 'white',
+  },
+  weatherDescription: {
+    fontFamily: "Noto Sans",
+    fontWeight: "400",
+    fontSize: '1.3em',
+    color: 'white',
+  },
+  humidity: {
+    fontFamily: "Noto Sans",
+    fontWeight: "400",
+    fontSize: '1em',
+    color: 'white',
+  },
+  weekWeather:  {
+
+  },
+  weekday: {
+    fontFamily: "Noto Sans",
+    fontWeight: "300",
+  },
+  minmaxTemperature: {
+    fontFamily: "Noto Sans",
+    fontWeight: "300",
+  }
+}))
 
 function App() {
+  const [location, setLocation] = useState("PLOVDIV");
+  const [currentTime, setCurrentTime] = useState("THURSDAY, 11:35am");
+  const [temperature, setTemperature] = useState("6°");
+  const [weatherDescription, setWeatherDescription] = useState("Mostly cloudy");
+  const [humidity, setHumidity] = useState("63%");
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xs" style={{border: "1px solid red"}}>
+      <Grid container direction='column' alignItems='center' className={classes.weatherInfoContainer}>
+        <Grid item className={classes.location}>
+          {location}
+        </Grid>
+        <Grid item className={classes.currentTime}>
+          {currentTime}
+        </Grid>
+        <Grid item className={classes.temperature}>
+          {temperature}
+        </Grid>
+        <Grid item className={classes.weatherDescription}>
+          {weatherDescription}
+        </Grid>
+        <Grid item className={classes.humidity}>
+          {humidity}
+        </Grid>
+      </Grid>
+      <Paper className={classes.weekWeather}>
+        <Grid container direction='column'>
+          <Grid item>
+            <Grid container>
+              <Grid item xs={2}>
+                Icon
+              </Grid>
+              <Grid item xs={7} className={classes.weekday}>
+                Tomorrow
+              </Grid>
+              <Grid item xs={3} className={classes.minmaxTemperature}>
+                2° / 6°
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
   );
 }
 
