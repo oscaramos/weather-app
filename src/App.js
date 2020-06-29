0f7e94e4e33a6d12058a97c69d46ae128768b923
Oscar <oscar.ramos@ucsp.edu.pe>
2020-06-29 18:36:19 -0500

    Add hooks for controlling loading and error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
 
 import React, { useEffect, useState } from 'react'
+import { useAsync } from '@react-hook/async'
+
 import Container from '@material-ui/core/Container'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 import Header from './components/Header'
 import Menu from './components/Menu'
@@ -14,78 +17,96 @@ import {
 
 import {
   getCurrentDay, getCurrentTime,
-  getWeekdayString
+  getWeekdayString,
 } from './timeController'
 
 import './App.css'
 
 function App() {
+  const promiseCurrentCondition = requestCurrentCondition()
+    .then((condition) => ({
+      temperature: condition.temperature,
+      weatherDescription: condition.weatherDescription,
+      humidity: condition.humidity,
+    }))
+
+  const promiseDayPredictions = requestDayMinMaxTemperatures('arequipa')
+    .then((dayTemperatures) => dayTemperatures.map((day, idx) => ({
+      minTemperature: day.minimum,
+      maxTemperature: day.maximum,
+      icon: getIconSvgFromIconNumber(day.icon),
+      weekDay: getWeekdayString(getCurrentDay() + idx),
+    })))
+
+  const promiseData = requestHourTemperatures('arequipa')
+    .then((hourPredictions) => [
+      {
+        id: 'weather',
+        data: hourPredictions.slice(0, 6)
+          .map((hourPrediction, index) => ({
+            x: `${12 + index}:00`,
+            y: hourPrediction.temperature,
+          })),
+      },
+    ])
+
+  const [{ status: headerStatus, value: headerData }, callHead] = useAsync(
+    () => Promise.all([promiseCurrentCondition, promiseData])
+      .then((values) => {
+        const currentCondition = values[0]
+        const data = values[1]
+        return {
+          ...currentCondition,
+          data,
+        }
+      }),
+  )
+
+  const [{ status: menuStatus, value: menuData }, callMenu] = useAsync(() => promiseDayPredictions)
+
   const [location] = useState('arequipa')
   const [currentTime] = useState(getCurrentTime())
-  const [temperature, setTemperature] = useState('')
-  const [weatherDescription, setWeatherDescription] = useState('')
-  const [humidity, setHumidity] = useState('')
-
-  const [weekWeather, setWeekWeather] = useState([])
-  const [data, setData] = useState([
-    {
-      id: 'weather',
-      data: [
-        {
-          x: '12:00',
-          y: 6,
-        },
-      ],
-    },
-  ])
 
   useEffect(() => {
-    requestDayMinMaxTemperatures('arequipa')
-      .then((dayTemperatures) => {
-        setWeekWeather(
-          dayTemperatures.map((day, idx) => ({
-            minTemperature: day.minimum,
-            maxTemperature: day.maximum,
-            icon: getIconSvgFromIconNumber(day.icon),
-            weekDay: getWeekdayString(getCurrentDay() + idx),
-          })),
-        )
-      })
-
-    requestHourTemperatures('arequipa')
-      .then((hourPredictions) => {
-        setData([
-          {
-            id: 'weather',
-            data: hourPredictions.slice(0, 6)
-              .map((hourPrediction, index) => ({
-                x: `${12 + index}:00`,
-                y: hourPrediction.temperature,
-              })),
-          },
-        ])
-      })
-
-    requestCurrentCondition('arequipa')
-      .then((condition) => {
-        setTemperature(condition.temperature)
-        setWeatherDescription(condition.weatherDescription)
-        setHumidity(condition.humidity)
-      })
+    callHead()
+    callMenu()
   }, [])
 
   return (
     <Container maxWidth="xs">
-      <Header
-        data={data}
-        location={location}
-        currentTime={currentTime}
-        temperature={temperature}
-        weatherDescription={weatherDescription}
-        humidity={humidity}
-      />
-
-      <Menu weekWeather={weekWeather} />
+      {
+        (() => {
+          switch (headerStatus) {
+            case 'success':
+              return (
+                <Header
+                  data={headerData.data}
+                  location={location}
+                  currentTime={currentTime}
+                  temperature={headerData.temperature}
+                  weatherDescription={headerData.weatherDescription}
+                  humidity={headerData.humidity}
+                />
+              )
+            case 'loading':
+              return <CircularProgress />
+            default:
+              return <div />
+          }
+        })()
+      }
+      {
+        (() => {
+          switch (menuStatus) {
+            case 'success':
+              return <Menu weekWeather={menuData} />
+            case 'loading':
+              return <CircularProgress />
+            default:
+              return <div />
+          }
+        })()
+      }
     </Container>
   )
 }
