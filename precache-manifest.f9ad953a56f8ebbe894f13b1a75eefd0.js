6c0928c0287fa8dd6d7708b1b852a56198523a43
Oscar <oscar.ramos@ucsp.edu.pe>
2020-06-30 12:42:57 -0500

    Updates

---
+++ precache-manifest.f9ad953a56f8ebbe894f13b1a75eefd0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df43838bca85259080aae8923b2bb1c0",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "8babd28330c43a9df119",
+    "url": "/weather-app/static/css/main.3c242fc9.chunk.css"
+  },
+  {
+    "revision": "ea8b427efe1810b57f10",
+    "url": "/weather-app/static/js/2.56980923.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.56980923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8babd28330c43a9df119",
+    "url": "/weather-app/static/js/main.839fd683.chunk.js"
+  },
+  {
+    "revision": "55676f7ecad5f9b6942e",
+    "url": "/weather-app/static/js/runtime-main.58bc7688.js"
+  },
+  {
+    "revision": "55e1764ac78314aad18ed3cba90868e3",
+    "url": "/weather-app/static/media/NotoSans-Light.55e1764a.ttf"
+  },
+  {
+    "revision": "f9493ba9866990680a59ac42835b01bb",
+    "url": "/weather-app/static/media/NotoSans-Regular.f9493ba9.ttf"
+  },
+  {
+    "revision": "390771ca0f5dc56ffc49b2838faf1895",
+    "url": "/weather-app/static/media/NotoSans-SemiBold.390771ca.ttf"
+  },
+  {
+    "revision": "172af5827f33194727a2f20a3cc94df1",
+    "url": "/weather-app/static/media/NotoSans-Thin.172af582.ttf"
+  },
+  {
+    "revision": "a78514970decb6855731eec3131803d2",
+    "url": "/weather-app/static/media/cloudy.a7851497.svg"
+  },
+  {
+    "revision": "fa73863b1980700d8287538592ad5b41",
+    "url": "/weather-app/static/media/foggy.fa73863b.svg"
+  },
+  {
+    "revision": "f86be2188d88d5f8e8583e89d2906995",
+    "url": "/weather-app/static/media/humidity.f86be218.svg"
+  },
+  {
+    "revision": "9ec5e190b92c56b682e544cc0dcd2694",
+    "url": "/weather-app/static/media/location.9ec5e190.svg"
+  },
+  {
+    "revision": "febd4c6b899e1f0c8d5833a0060e6791",
+    "url": "/weather-app/static/media/partly-cloudy.febd4c6b.svg"
+  },
+  {
+    "revision": "7879a4fb4fab005ab4d2060326b134c2",
+    "url": "/weather-app/static/media/rainy.7879a4fb.svg"
+  },
+  {
+    "revision": "4a1a81a4f61a1948096e244f71532319",
+    "url": "/weather-app/static/media/snowdy.4a1a81a4.svg"
+  },
+  {
+    "revision": "80ae3d87c34d4d5c474d132823d8edd8",
+    "url": "/weather-app/static/media/sunny.80ae3d87.svg"
+  }
+]);
